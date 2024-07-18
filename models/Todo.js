ee8385e0dd9e20123c3ac5a1eb042e5bc9c7f231
Tanzeel Khan <tanzeelk.909@gmail.com>
2024-07-18 01:51:23 -0400

    Integrated pomodoro with todo

--- models/Todo.js
+++ models/Todo.js
@@ -4,7 +4,11 @@ const todoSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   text: { type: String, required: true },
   completed: { type: Boolean, default: false },
-  createdAt: { type: Date, default: Date.now }
+  createdAt: { type: Date, default: Date.now },
+  timeSpent: {
+    type: Number,
+    default: 0 // Time spent in minutes
+  }
 });
 
 const Todo = mongoose.model('Todo', todoSchema);
