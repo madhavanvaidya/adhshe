7311ce544c67162b079859aca09d2528df162920
SHIVANGI1SINGH <150862330+SHIVANGI1SINGH@users.noreply.github.com>
2024-08-05 16:26:43 -0400

    Create menstruation.js

---
+++ models/menstruation.js
@@ -0,0 +1,14 @@
+models : const mongoose = require('mongoose');
+
+const MenstrualCycleSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, required: true },
+  startDate: { type: Date, required: true },
+  cycleLength: { type: Number, required: true },
+  symptoms: { type: [String], required: true },
+  mood: { type: String, required: true }
+});
+
+// Adding indexes
+MenstrualCycleSchema.index({ userId: 1, startDate: -1 });
+
+module.exports = mongoose.model('MenstrualCycle', MenstrualCycleSchema);
