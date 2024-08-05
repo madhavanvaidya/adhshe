c07878e9b50d7bd9d44eb1a6242e701d697ae2d9
SHIVANGI1SINGH <150862330+SHIVANGI1SINGH@users.noreply.github.com>
2024-08-05 14:18:14 -0400

    Update mensural cycle.js

--- models/mensural cycle.js
+++ models/mensural cycle.js
@@ -4,7 +4,8 @@ const MenstrualCycleSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, required: true },
   startDate: { type: Date, required: true },
   cycleLength: { type: Number, required: true },
-  symptoms: { type: [String], required: true } // Array of symptoms
+  symptoms: { type: [String], required: true }, // Array of symptoms
+  mood: { type: String, required: true } // Mood field
 });
 
 module.exports = mongoose.model('MenstrualCycle', MenstrualCycleSchema);
